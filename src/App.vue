8be1509858199317de971e9883781534ceb1a789
unknown <songwl@sanmiaofz.com>
2018-12-10 17:16:35 +0800

    build project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#demo {
+  font-family: 'Helvetica', Arial, sans-serif;
+}
+a {
+  text-decoration: none;
+  color: #f66;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author, .date {
+  font-weight: bold;
+}
+</style>
